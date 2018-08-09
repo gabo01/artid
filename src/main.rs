ea0a0863483b6fb5bea4ba1773d3d1b97d869d3e
Gabriel Dos Ramos Albo <dosramosgabriel@gmail.com>
2018-08-09 02:37:22 +0200

    Basic app structure, removed actix

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
-extern crate actix;
 extern crate backup_rs; // app
-extern crate libc;
 #[macro_use]
-extern crate log;
+extern crate clap;
+extern crate libc;
 
 use backup_rs::logger::term;
+use clap::ArgMatches;
 use libc::EXIT_FAILURE;
-
 use std::process::exit;
 
 fn main() {
@@ -15,8 +14,18 @@ fn main() {
         exit(EXIT_FAILURE);
     }
 
-    debug!("This is a debug message");
+    let yaml = load_yaml!("cli.yml");
+    App::new(clap::App::from(yaml).get_matches()).run();
+}
 
-    let system = actix::System::new("backup-rs");
-    system.run();
+struct App<'a> {
+    _matches: ArgMatches<'a>,
+}
+
+impl<'a> App<'a> {
+    pub fn new(_matches: ArgMatches<'a>) -> Self {
+        App { _matches }
+    }
+
+    pub fn run(self) {}
 }
