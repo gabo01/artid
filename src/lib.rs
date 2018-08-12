15947aea774978f6bf427fe9e15e3b9f2494c9e5
Gabriel Dos Ramos Albo <dosramosgabriel@gmail.com>
2018-08-12 14:53:25 +0200

    Updated ConfigFile and removed description from Folder
    Added update of the backup date  to the config file
    Removed unnecessary traits from ConfigFile

--- src/lib.rs
+++ src/lib.rs
@@ -21,6 +21,7 @@ use chrono::DateTime;
 use env_path::EnvPath;
 use failure::ResultExt;
 use std::fs::File;
+use std::io::Write;
 use std::path::{Path, PathBuf};
 
 pub mod errors;
@@ -39,16 +40,22 @@ macro_rules! err {
 
 pub type Result<T> = ::std::result::Result<T, AppError>;
 
-#[derive(Debug, Serialize, Deserialize)]
-pub struct ConfigFile {
+pub struct ConfigFile<P>
+where
+    P: AsRef<Path>,
+{
+    path: P,
     folders: Vec<Folder>,
 }
 
-impl ConfigFile {
+impl<P> ConfigFile<P>
+where
+    P: AsRef<Path>,
+{
     const RESTORE: &'static str = ".backup/config.json";
 
-    pub fn load<P: AsRef<Path>>(path: P) -> Result<Self> {
-        let file = Self::filepath(path)?;
+    pub fn load(path: P) -> Result<Self> {
+        let file = Self::filepath(&path)?;
 
         let reader =
             File::open(&file).context(AppErrorType::AccessFile(file.display().to_string()))?;
@@ -56,26 +63,60 @@ impl ConfigFile {
             .context(AppErrorType::JsonParse(file.display().to_string()))?;
         trace!("{:?}", folders);
 
-        Ok(ConfigFile { folders })
+        Ok(ConfigFile { path, folders })
     }
 
-    pub fn backup<P: AsRef<Path>>(self, root: P) -> Result<()> {
-        for folder in self {
-            let dirs = folder.resolve(&root);
-            debug!("Starting backup of: {}", pathlight(&dirs.abs));
+    pub fn save<T: AsRef<Path>>(&self, location: T) -> Result<()> {
+        let path = location.as_ref();
+        write!(
+            File::create(&path).context(AppErrorType::AccessFile(path.display().to_string()))?,
+            "{}",
+            json::to_string_pretty(self.folders).expect("ConfigFile cannot fail serialization")
+        ).context(AppErrorType::AccessFile(path.display().to_string()))?;
 
-            LinkTree::new(dirs.rel, dirs.abs)
-                .sync()
-                .context(AppErrorType::UpdateFolder(
-                    root.as_ref().display().to_string(),
-                ))?;
-        }
-
+        info!("Config file saved on {}", pathlight(self.path.as_ref()));
         Ok(())
     }
 
-    pub fn restore<P: AsRef<Path>>(self, root: P) -> Result<()> {
-        for folder in self {
+    pub fn save_default(&self) -> Result<()> {
+        self.save(Self::filepath(&self.path)?)
+    }
+
+    pub fn backup<T: AsRef<Path>>(mut self, root: T) -> Result<()> {
+        let mut error = None;
+        for folder in &mut self.folders {
+            let dirs = folder.resolve(&root);
+            debug!("Starting backup of: {}", pathlight(&dirs.abs));
+
+            if let Err(err) =
+                LinkTree::new(dirs.rel, dirs.abs)
+                    .sync()
+                    .context(AppErrorType::UpdateFolder(
+                        root.as_ref().display().to_string(),
+                    )) {
+                error = Some(err);
+                break;
+            }
+
+            folder.modified = Some(Utc::now());
+        }
+
+        if let Err(err) = self.save_default() {
+            warn!(
+                "Unable to save on {} because of {}",
+                pathlight(self.path.as_ref()),
+                err
+            );
+        }
+
+        match error {
+            Some(err) => err!(err.into()),
+            None => Ok(()),
+        }
+    }
+
+    pub fn restore<T: AsRef<Path>>(self, root: T) -> Result<()> {
+        for folder in &self.folders {
             let dirs = folder.resolve(&root);
             debug!("Starting restore of: {}", pathlight(&dirs.rel));
 
@@ -89,7 +130,7 @@ impl ConfigFile {
         Ok(())
     }
 
-    fn filepath<P: AsRef<Path>>(path: P) -> Result<PathBuf> {
+    fn filepath<T: AsRef<Path>>(path: T) -> Result<PathBuf> {
         let path = path.as_ref();
 
         if !path.is_dir() {
@@ -110,29 +151,10 @@ impl ConfigFile {
     }
 }
 
-impl IntoIterator for ConfigFile {
-    type Item = Folder;
-    type IntoIter = std::vec::IntoIter<Folder>;
-
-    fn into_iter(self) -> Self::IntoIter {
-        self.folders.into_iter()
-    }
-}
-
-impl<'a> IntoIterator for &'a ConfigFile {
-    type Item = &'a Folder;
-    type IntoIter = std::slice::Iter<'a, Folder>;
-
-    fn into_iter(self) -> Self::IntoIter {
-        self.folders.iter()
-    }
-}
-
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Folder {
     path: EnvPath,
     origin: EnvPath,
-    description: String,
     modified: Option<DateTime<Utc>>, // parses from an RFC3339 valid string
 }
 
