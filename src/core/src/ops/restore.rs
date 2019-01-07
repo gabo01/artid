7d6b9c7ad2344a20a1bda08d1e523aca627ae3bf
Gabriel Dos Ramos Albo <dosramosgabriel@gmail.com>
2019-01-07 22:29:55 +0100

    tests: add tests for restore
    These tests are taken from the original ones in the config module

--- src/core/src/ops/restore.rs
+++ src/core/src/ops/restore.rs
@@ -170,3 +170,68 @@ fn actions(folder: &FileSystemFolder, options: Options) -> Result<Actions, Build
         Ok(vec![])
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use chrono::Utc;
+    use std::path::{Path, PathBuf};
+    use std::thread;
+    use std::time;
+    use tempfile::TempDir;
+
+    use super::super::test_helpers::{FileKind, FileTree};
+    use super::{Model, Operator, Options, Restore};
+    use prelude::{FileSystemFolder, FolderConfig};
+
+    macro_rules! backup {
+        ($root:ident, $stamp:ident, $generate:expr) => {{
+            let format = format!("backup/{}", rfc3339!($stamp));
+            let path = tmppath!($root, format);
+            if $generate {
+                FileTree::generate_from(path)
+            } else {
+                FileTree::new(path)
+            }
+        }};
+    }
+
+    #[test]
+    fn test_folder_restore_single() {
+        let mut origin = FileTree::create();
+        let (root, stamp) = (tmpdir!(), Utc::now());
+        let backup = backup!(root, stamp, true);
+
+        let options = Options::default();
+        let mut config = FolderConfig::new("backup", origin.path());
+        config.add_modified(stamp); // in order to detect the backup
+        let mut folder = config.apply_root(root.path());
+        run!(folder, options, Restore);
+
+        origin.copy_tree(&backup);
+        origin.assert();
+    }
+
+    #[test]
+    #[ignore]
+    fn test_folder_restore_with_symlinks() {
+        let mut origin = FileTree::create();
+        let (root, stamp) = (tmpdir!(), Utc::now());
+        let backup = backup!(root, stamp, true);
+
+        thread::sleep(time::Duration::from_millis(2000));
+        let stamp_new = Utc::now();
+        let mut backup_second = backup!(root, stamp_new, false);
+        backup_second.add_root();
+        backup_second.add_symlink("a.txt", backup.path().join("a.txt"));
+        backup_second.add_symlink("b.txt", backup.path().join("b.txt"));
+
+        let options = Options::default();
+        let mut config = FolderConfig::new("backup", origin.path());
+        config.add_modified(stamp_new); // in order to detect the last backup
+        let mut folder = config.apply_root(root.path());
+        run!(folder, options, Restore);
+
+        origin.copy_tree(&backup);
+        origin.assert();
+    }
+}
