d5b5f803239d6f4ec1b5f9b5e0f4892ae566c111
Gabriel Dos Ramos Albo <dosramosgabriel@gmail.com>
2019-01-07 22:24:49 +0100

    feat: model the restore operation based on the new principles

---
+++ src/core/src/ops/restore.rs
@@ -0,0 +1,172 @@
+//! Holds all the logic for performing a restore operation
+//!
+//! The easiest way to use this module is through the global helper 'restore'. The restore
+//! function will return the associated restore model for the given operator, meaning that
+//! the actual model returned may vary based on the operator. The current operators are:
+//!
+//! - ConfigFile<P>: will return a model to execute a new restore for every registered folder
+//!   and fail if any of the singular models fails to build.
+//! - FileSystemFolder: will return a model to execute a restore on the singular folder.
+//!   calling this model for every folder is equivalent to performing a mass restore
+
+use chrono::{DateTime, Utc};
+use failure::{Backtrace, Context, Fail, ResultExt};
+use std::fmt::{self, Debug, Display};
+use std::io;
+use std::path::Path;
+
+use super::{
+    core::{self, Actions, CopyAction, CopyModel, MultipleCopyModel},
+    Model, Operation, Operator,
+};
+use prelude::{ConfigFile, FileSystemFolder};
+
+/// Underlying kind of error responsible for failing the building of the restore model
+#[derive(Debug, Fail)]
+pub enum BuildErrorKind {
+    #[allow(missing_docs)]
+    #[fail(display = "The folder does not have the requested restore point")]
+    PointNotExists,
+    #[allow(missing_docs)]
+    #[fail(display = "Error while building the model from disk, see the cause for details")]
+    FsError,
+}
+
+/// Error that can be returned when building a restore model
+#[derive(Debug)]
+pub struct BuildError {
+    inner: Context<BuildErrorKind>,
+}
+
+impl Display for BuildError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        Display::fmt(&self.inner, f)
+    }
+}
+
+impl Fail for BuildError {
+    fn cause(&self) -> Option<&Fail> {
+        self.inner.cause()
+    }
+
+    fn backtrace(&self) -> Option<&Backtrace> {
+        self.inner.backtrace()
+    }
+}
+
+impl From<BuildErrorKind> for BuildError {
+    fn from(kind: BuildErrorKind) -> Self {
+        Self {
+            inner: Context::new(kind),
+        }
+    }
+}
+
+impl From<Context<BuildErrorKind>> for BuildError {
+    fn from(inner: Context<BuildErrorKind>) -> Self {
+        Self { inner }
+    }
+}
+
+/// Modifier options for the restore operation
+#[derive(Copy, Clone, Debug)]
+pub struct Options {
+    overwrite: bool,
+    point: Option<usize>,
+}
+
+impl Default for Options {
+    fn default() -> Self {
+        Self {
+            overwrite: false,
+            point: None,
+        }
+    }
+}
+
+/// Represents the restore operation. It's purpouse is to be the operation called for
+/// <Type as Operator<Operation>>::modelate(...)
+pub struct Restore;
+
+impl Restore {
+    fn from_point(restore: &Path, backup: &Path, overwrite: bool) -> Result<Actions, io::Error> {
+        use self::core::{DirTree, FileType, Presence};
+
+        let tree = DirTree::new(&restore, &backup)?;
+        Ok(tree
+            .iter()
+            .filter(|e| {
+                e.presence() == Presence::Dst
+                    || overwrite && e.presence() == Presence::Both && e.kind() != FileType::Dir
+            })
+            .map(|e| {
+                if e.kind() == FileType::Dir && e.presence() == Presence::Dst {
+                    CopyAction::CreateDir {
+                        target: restore.join(e.path()),
+                    }
+                } else {
+                    CopyAction::CopyFile {
+                        src: backup.join(e.path()),
+                        dst: restore.join(e.path()),
+                    }
+                }
+            })
+            .collect())
+    }
+}
+
+impl Operation for Restore {}
+
+impl<'mo, P: AsRef<Path> + Debug> Operator<'mo, Restore> for ConfigFile<P> {
+    type Model = MultipleCopyModel<'mo>;
+    type Error = BuildError;
+    type Options = Options;
+
+    fn modelate(&'mo mut self, options: Self::Options) -> Result<Self::Model, Self::Error> {
+        let dir = &self.dir;
+
+        Ok(MultipleCopyModel::new(
+            self.folders
+                .iter_mut()
+                .map(|e| {
+                    let folder = e.apply_root(&dir);
+                    Ok(CopyModel::new(actions(&folder, options)?, || {}))
+                })
+                .collect::<Result<_, BuildError>>()?,
+        ))
+    }
+}
+
+impl<'mo> Operator<'mo, Restore> for FileSystemFolder<'mo> {
+    type Model = CopyModel<'mo>;
+    type Error = BuildError;
+    type Options = Options;
+
+    fn modelate(&'mo mut self, options: Self::Options) -> Result<Self::Model, Self::Error> {
+        Ok(CopyModel::new(actions(&self, options)?, || {}))
+    }
+}
+
+fn actions(folder: &FileSystemFolder, options: Options) -> Result<Actions, BuildError> {
+    if folder.config.has_sync() {
+        let modified = match options.point {
+            Some(point) => folder.config.find_sync(point),
+            None => folder.config.find_last_sync(),
+        };
+
+        if let Some(modified) = modified {
+            debug!("Starting restore of: {}", folder.link.relative.display());
+            let relative = folder.link.relative.join(rfc3339!(modified));
+
+            Ok(
+                Restore::from_point(&folder.link.origin, &relative, options.overwrite)
+                    .context(BuildErrorKind::FsError)?,
+            )
+        } else {
+            Err(BuildErrorKind::PointNotExists)?
+        }
+    } else {
+        info!("Restore not needed for {}", folder.link.relative.display());
+        Ok(vec![])
+    }
+}
