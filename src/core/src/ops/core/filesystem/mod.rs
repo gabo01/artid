39f096680010999e7868aed7295ab6f6556b63b6
Gabriel Dos Ramos Albo <dosramosgabriel@gmail.com>
2019-01-14 23:09:07 +0100

    refactor: generalize the remaining elements of the filesystem traits

--- src/core/src/ops/core/filesystem/mod.rs
+++ src/core/src/ops/core/filesystem/mod.rs
@@ -1,6 +1,8 @@
+use std::ffi::OsString;
 use std::fs::{self, OpenOptions};
 use std::io;
 use std::path::{Display, Path, PathBuf};
+use std::time::SystemTime;
 
 mod local;
 
@@ -13,25 +15,37 @@ pub use self::local::Local;
 /// intended to behave in a similar way to the ones implemented there
 pub trait FileSystem: Route {
     #[allow(missing_docs)]
+    type File: File<Metadata = Self::Metadata>;
+
+    #[allow(missing_docs)]
+    type Metadata: Metadata;
+
+    #[allow(missing_docs)]
+    type Directory: Directory;
+
+    #[allow(missing_docs)]
+    type DirectoryIterator: DirectoryIterator<Self::Directory>;
+
+    #[allow(missing_docs)]
     fn new<P: Into<PathBuf>>(path: P) -> Self;
 
     #[allow(missing_docs)]
     fn exists(&self) -> bool;
 
     #[allow(missing_docs)]
-    fn metadata(&self) -> io::Result<fs::Metadata>;
+    fn metadata(&self) -> io::Result<Self::Metadata>;
 
     #[allow(missing_docs)]
-    fn symlink_metadata(&self) -> io::Result<fs::Metadata>;
+    fn symlink_metadata(&self) -> io::Result<Self::Metadata>;
 
     #[allow(missing_docs)]
     fn is_file(&self) -> bool;
 
     #[allow(missing_docs)]
-    fn open(&self, options: &OpenOptions) -> io::Result<fs::File>;
+    fn open(&self, options: &OpenOptions) -> io::Result<Self::File>;
 
     #[allow(missing_docs)]
-    fn read_dir(&self) -> io::Result<fs::ReadDir>;
+    fn read_dir(&self) -> io::Result<Self::DirectoryIterator>;
 
     #[allow(missing_docs)]
     fn create_dir_all(&self) -> io::Result<()>;
@@ -73,4 +87,49 @@ pub trait Route {
 
     #[allow(missing_docs)]
     fn display(&self) -> Display<'_>;
+}
+
+/// Representation of the open file objects inside a filesystem
+pub trait File: io::Read + io::Write {
+    #[allow(missing_docs)]
+    type Metadata: Metadata;
+
+    #[allow(missing_docs)]
+    fn metadata(&self) -> io::Result<Self::Metadata>;
+}
+
+/// Representation of the metadata inside a path for a particular filesystem
+pub trait Metadata {
+    #[allow(missing_docs)]
+    type FileKind: FileKind;
+
+    #[allow(missing_docs)]
+    fn file_type(&self) -> Self::FileKind;
+
+    #[allow(missing_docs)]
+    fn modified(&self) -> io::Result<SystemTime>;
+}
+
+/// Representation of the filekind object for a particular filesystem
+pub trait FileKind {
+    #[allow(missing_docs)]
+    fn is_symlink(&self) -> bool;
+
+    #[allow(missing_docs)]
+    fn is_file(&self) -> bool;
+
+    #[allow(missing_docs)]
+    fn is_dir(&self) -> bool;
+}
+
+#[allow(missing_docs)]
+pub trait DirectoryIterator<D: Directory>: Iterator<Item = io::Result<D>> {}
+
+/// Representation of a directory in the filesystem
+pub trait Directory {
+    #[allow(missing_docs)]
+    fn path(&self) -> PathBuf;
+
+    #[allow(missing_docs)]
+    fn file_name(&self) -> OsString;
 }
